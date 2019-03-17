ed2a5e1f51956ba4cbd88dd9c0e2093693eb49fc
Natalie Brickley <natt_rose@yahoo.com>
2019-03-17 12:46:29 -0700

    concert-this command has been coded

--- liri.js
+++ liri.js
@@ -6,11 +6,29 @@ var moment = require("moment");
 
 //var spotify = new Spotify(keys.spotify);
 
+//check to see what kind of request is being made:
 switch (process.argv[2]) {
+    //if we want a concert: 
     case "concert-this":
-        var artist = process.argv[3];
+        //get the artists full name:
+        var artist = "";
+        for (i=3; i<process.argv.length; i++) {
+            artist += process.argv[i];
+        }
+        //generate our query url:
         var bandsInTownURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+        //make our api request:
         axios.get(bandsInTownURL).then(function(response){
-            console.log(bandsInTownURL)
+            for(var i=0; i<response.data.length; i++) {
+                //get the venue
+                var venue = "Venue: " + response.data[i].venue.name;
+                //get the location
+                var location = "Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region;
+                //get the date and format it with moment.js
+                var formatDate = moment(response.data[i].datetime).format("MM/DD/YYYY")
+                var date = "Date: " + formatDate;
+                //log the results
+                console.log(venue, location, date)
+            }
         })
 }
