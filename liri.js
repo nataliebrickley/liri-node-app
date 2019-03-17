87f6c29550597c1eab47c47fd92f6b67484bfe3c
Natalie Brickley <natt_rose@yahoo.com>
2019-03-17 11:56:44 -0700

    installed moment, spotify, and dotenv

--- liri.js
+++ liri.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
+require("node-spotify-api")
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var moment = require("moment");
+
+//var spotify = new Spotify(keys.spotify);
 
 switch (process.argv[2]) {
-    case concert-this:
+    case "concert-this":
         var artist = process.argv[3];
         var bandsInTownURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-        
+        axios.get(bandsInTownURL).then(function(response){
+            console.log(bandsInTownURL)
+        })
 }
